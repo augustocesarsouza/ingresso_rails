ec4a2fd9f608d01e66818db996f00395958ab9d3
augustocesarsouza <augustocesarsantana90@gmail.com>
2024-03-22 16:27:03 -0400

    algumas alterações application algumas validações

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+  include Pundit::Authorization
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation, :phone])
+  end
+
+  private
+  
+  def user_not_authorized(exception)
+    policy_name = exception.policy.class.to_s.underscore
+
+    flash[:notice] = t "#{policy_name}.#{exception.query}", scope: 'pundit', default: :default
+    redirect_back(fallback_location: admin_root_path)
+  end
 end
