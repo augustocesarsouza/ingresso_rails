320b2564151673bd9de297b90cba1ba92315879a
augustocesarsouza <augustocesarsantana90@gmail.com>
2024-03-22 15:46:25 -0400

    criando jquery, mask, os stimulus admin_modal e modal, ajustando index.js, e criando html footer nav user_signed

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,6 +6,12 @@ import { application } from "./application"
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
+import AdminModalController from "./admin_modal_controller"
+application.register("admin-modal", AdminModalController)
+
+import ModalController from "./modal_controller"
+application.register("modal", AdminModalController)
+
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 // lazyLoadControllersFrom("controllers", application)
