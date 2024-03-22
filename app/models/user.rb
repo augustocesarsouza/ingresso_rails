c3f5c931b05db5cdbefd8b9889e6de6bb85f3bb4
augustocesarsouza <augustocesarsantana90@gmail.com>
2024-03-22 11:59:05 -0400

    Gerando as views do devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, :phone, presence: true
+
+  has_one_attached :avatar
+
+  enum role: { user: 'user', admin: 'admin'}
 end
