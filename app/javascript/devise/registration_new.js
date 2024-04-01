4fc42dbe0d4cfa8bf27ed71ac3e93d4633c86298
augustocesarsouza <augustocesarsantana90@gmail.com>
2024-04-01 08:51:11 -0400

    arrumando new user, e additional_user, css

--- app/javascript/devise/registration_new.js
+++ app/javascript/devise/registration_new.js
@@ -40,10 +40,22 @@ const functionGetClickKeyboard = (event) => {
   }, 10);
 }
 
+if(inputPassword){
+  inputPassword.addEventListener('keydown', functionGetClickKeyboard);
+}
+
 document.addEventListener('DOMContentLoaded', function() {
   const birthYear = document.getElementById("user_additional_info_user_attributes_birth_date_1i");
   const birthMonth = document.getElementById("user_additional_info_user_attributes_birth_date_2i");
   const birthDay = document.getElementById("user_additional_info_user_attributes_birth_date_3i");
+
+  if(birthYear == null || birthMonth == null || birthDay == null) return;
+
+  createOptionYearCustom(birthYear);
+
+  createOptionMonthCustom(birthMonth);
+
+  createOptionDayCustom(birthDay);
 
   birthMonth.addEventListener("change", function() {
     updateDays();
@@ -56,7 +68,6 @@ document.addEventListener('DOMContentLoaded', function() {
     birthDay.innerHTML = '';
 
     var lastDayOfMonth = new Date(year, month, 0).getDate();
-    console.log(lastDayOfMonth);
     
     for (var day = 1; day <= lastDayOfMonth; day++) {
       var option = document.createElement("option");
@@ -66,3 +77,30 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   }
 });
+
+const createOptionYearCustom = (birthYear) => {
+  const optionYear = document.createElement("option");
+  optionYear.text = 'Year of Birth';
+  optionYear.value = 0;
+  optionYear.selected = true;
+
+  birthYear.insertBefore(optionYear, birthYear.firstChild);
+}
+
+const createOptionMonthCustom = (birthMonth) => {
+  const optionMonth = document.createElement("option");
+  optionMonth.text = 'Month the of Birth';
+  optionMonth.value = 0;
+  optionMonth.selected = true;
+
+  birthMonth.insertBefore(optionMonth, birthMonth.firstChild);
+}
+
+const createOptionDayCustom = (birthDay) => {
+  const optionDay = document.createElement("option");
+  optionDay.text = 'Day the of Birth';
+  optionDay.value = 0;
+  optionDay.selected = true;
+
+  birthDay.insertBefore(optionDay, birthDay.firstChild);
+}
