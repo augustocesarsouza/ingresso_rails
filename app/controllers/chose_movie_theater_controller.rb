67cc58c0e017c5aeb816cba92eaa621f06c79ed1
augustocesarsouza <augustocesarsantana90@gmail.com>
2024-04-06 15:16:23 -0400

    ajeitando chose_movie_theater

--- app/controllers/chose_movie_theater_controller.rb
+++ app/controllers/chose_movie_theater_controller.rb
@@ -2,8 +2,16 @@ class ChoseMovieTheaterController < ApplicationController
   def index
     movie_id = params[:id]
     @movie = Movie.select(:id, :title, :gender, :duration, :movie_rating, :description).where(id: movie_id).first
-    print '****************************************************************'
-    print @movie.inspect
-    print '****************************************************************'
+    @cinemas = Cinema.select(:id, :name_cinema, :district, :ranking).all
+    
   end
 end
+
+
+# create_table "cinemas", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+#   t.string "name_cinema", limit: 150, null: false
+#   t.string "district", limit: 150, null: false
+#   t.string "ranking", limit: 150, null: false
+#   t.datetime "created_at", null: false
+#   t.datetime "updated_at", null: false
+# end
