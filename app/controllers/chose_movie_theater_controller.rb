df21376375177d1112980f59b6ae4c97d92c5c37
augustocesarsouza <augustocesarsantana90@gmail.com>
2024-04-07 13:13:12 -0400

    alteração chose_movie_theater index

--- app/controllers/chose_movie_theater_controller.rb
+++ app/controllers/chose_movie_theater_controller.rb
@@ -1,17 +1,18 @@
 class ChoseMovieTheaterController < ApplicationController
   def index
     movie_id = params[:id]
+    region_city_name = "Campinas";
     @movie = Movie.select(:id, :title, :gender, :duration, :movie_rating, :description).where(id: movie_id).first
+    @region_city_id = Region.select(:id).where(city: region_city_name).first;
     @cinemas = Cinema.select(:id, :name_cinema, :district, :ranking).all
-    
+
+    # for cn in @cinemas
+    #   # puts cn.id
+    #   @cinemas_movies = CinemaMovie.select(:screening_schedule).where(movie_id: movie_id, cinema_id: cn.id, region_id: @region_city_id.id);
+    #   puts
+    #   puts '********************************************************************************************************************************'
+    #   puts @cinemas_movies.count
+    #   puts '********************************************************************************************************************************'
+    # end
   end
 end
-
-
-# create_table "cinemas", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-#   t.string "name_cinema", limit: 150, null: false
-#   t.string "district", limit: 150, null: false
-#   t.string "ranking", limit: 150, null: false
-#   t.datetime "created_at", null: false
-#   t.datetime "updated_at", null: false
-# end
