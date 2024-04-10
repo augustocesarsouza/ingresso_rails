effa43c2391f3dc8ddcfb68aaa11ec0acbfa4230
augustocesarsouza <augustocesarsantana90@gmail.com>
2024-04-10 16:36:59 -0400

    terminei o filtro para a category para filmes sala de cinema

--- app/javascript/chose_movie_theater/index.js
+++ app/javascript/chose_movie_theater/index.js
@@ -97,12 +97,15 @@ if(containerListCategory && containerListCategory.children){
       funcClickElementCategory(element);
     });
   }
-  
+  // Filtro para category movie / START
   let arrayElementClick = [];
 
   let containerMainHourCategory = document.querySelectorAll(".container-schedule-movie-theater");
 
-  let containerForDelete = []; // tem que ver se vai ter que deixar dentro da função click ou nao
+  let elementContainerMainDelete = [];
+  let elementContainerMainChilgrenDelete = [];
+
+  let arrayAllTextSpan = [];
 
   const funcClickElementCategory = (element) => {
     if(arrayElementClick.includes(element.textContent)){
@@ -112,40 +115,74 @@ if(containerListCategory && containerListCategory.children){
       arrayElementClick.push(element.textContent);
     }
 
-    containerMainHourCategory.forEach((containerMain) => {
-      for (let i = 0; i < containerMain.children.length; i++) {
-        const element = containerMain.children[i]; // se o span nao tiver o valor dentro desse array "arrayElementClick" remover
-        
-        let containerSpan = element.querySelector(".container-span-telasgigantesplf-dublado");
-        
-        let valorRef = false;
-        
-        for (let j = 0; j < containerSpan.children.length; j++) {
-          let elementSpan = containerSpan.children[j];
+    elementContainerMainChilgrenDelete.forEach((elDelete) => {
+      elDelete.style.display = "flex";
+    });
 
-          if(arrayElementClick.includes(elementSpan.textContent)){
-            valorRef = true;
-          }
+    elementContainerMainDelete.forEach((elMainDelete) => {
+      elMainDelete.style.display = "block";
+    });
 
-          if (j === containerSpan.children.length - 1) {
-            if (!valorRef) {
-              containerForDelete.push(element);
-              valorRef = true;
+    elementContainerMainDelete = [];
+    elementContainerMainChilgrenDelete = [];
+    
+    let inicioForCheckDisplay = 0;
+
+    if(arrayElementClick.length > 0) {
+      containerMainHourCategory.forEach((containerMain) => {
+        for (let i = 0; i < containerMain.children.length; i++) {
+          const element = containerMain.children[i];
+          
+          let containerSpan = element.querySelector(".container-span-telasgigantesplf-dublado");
+          
+          for (let j = 0; j < containerSpan.children.length; j++) {
+            let elementSpan = containerSpan.children[j];
+
+            arrayAllTextSpan.push(elementSpan.textContent);
+
+            // essa parte é para deixar display none se os elementos escolhidos aqui 'arrayElementClick' nao corresponder com os spans 'arrayAllTextSpan'
+            if(j === containerSpan.children.length - 1){
+              let countIfInclude = 0;
+              valueForDeleteParentSpan = false;
+
+              arrayAllTextSpan.forEach(textSpan => {
+                if(arrayElementClick.includes(textSpan)){
+                  countIfInclude++;
+                  
+                  if(countIfInclude >= arrayElementClick.length){
+                    valueForDeleteParentSpan = true;
+                    countIfInclude = 0;
+                  }
+                }
+              })
+
+              arrayAllTextSpan = [];
+
+              if (!valueForDeleteParentSpan) {
+                elementContainerMainChilgrenDelete.push(element);
+          
+                element.style.display = 'none';
+
+                valueForDeleteParentSpan = true;
+              }
             }
           }
+
+          // essa parte é para colocar display = none; nó container main se nenhum dos filhos dele estiver 'none'
+          if(element.style.display === 'none') {
+            inicioForCheckDisplay++;
+          }
+
+          if(containerMain.children.length === inicioForCheckDisplay){
+            elementContainerMainDelete.push(containerMain.parentElement);
+
+            containerMain.parentElement.style.display = 'none';
+          }
         }
-      }
-
-      containerForDelete.forEach((element) => {
-        element.remove();
+        inicioForCheckDisplay = 0;
       });
+    }
 
-      if(containerMain.children.length === 0){
-        containerMain.parentElement.remove();
-      }
-      
-    });
-  
     if(element.style.borderColor === "transparent"){
       element.style.color = "#98aaec";
       element.style.borderColor = "#98aaec";
@@ -155,11 +192,10 @@ if(containerListCategory && containerListCategory.children){
       element.style.color = "#FFF";
       element.style.borderColor = "transparent";
       element.style.background = "rgb(50 85 226)";
-
     }
-    // console.log(element.style.borderColor);
   }
 }
+// END
 
 const containerSvgTickets = document.querySelectorAll('.container-svg-ticket');
 
