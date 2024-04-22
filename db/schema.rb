48a143ee96b4e3cae1c62702c8d64493d406513d
augustocesarsouza <augustocesarsantana90@gmail.com>
2024-04-22 12:53:29 -0400

    criando migrate para criar table additional_food_movie

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_22_115619) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_22_164657) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,6 +44,18 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_22_115619) do
     t.uuid "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "additional_food_movies", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "title", limit: 100, null: false
+    t.string "price", limit: 70, null: false
+    t.string "fee", limit: 60, null: false
+    t.uuid "movie_id", null: false
+    t.uuid "cinema_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cinema_id"], name: "index_additional_food_movies_on_cinema_id"
+    t.index ["movie_id"], name: "index_additional_food_movies_on_movie_id"
   end
 
   create_table "additional_info_users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
@@ -139,6 +151,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_22_115619) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "additional_food_movies", "cinemas"
+  add_foreign_key "additional_food_movies", "movies"
   add_foreign_key "additional_info_users", "users"
   add_foreign_key "cinema_movies", "cinemas"
   add_foreign_key "cinema_movies", "movies"
